d5d32587971c48039da189be92008d2c948cd156
jadha <jadha@LAPTOP-QDSGVG2U>
2025-03-17 20:58:45 +0530

    Day 1

---
+++ Day_1/Indentation.py
@@ -0,0 +1,10 @@
+######################################################
+
+# 4 spaces are used in indentation.
+#By Indentation we can get the block of code.
+
+
+a = 'b'
+
+if a == 'b':
+    print ("Yes")  #4 spaces are used in indentation.
---
+++ Day_1/Variables_Datatype.py
@@ -0,0 +1,13 @@
+x = 10          # Integer
+name = "Alice"  # String
+pi = 3.14       # Float
+is_valid = True # Boolean
+
+print(x, name, pi, is_valid)
+
+user_name = input("Enter your name:")
+user_age = int(input("Enter your age:"))
+next_year= 1 + user_age
+
+print(f"Hello {user_name}! Next year, you will be {next_year} years old.")
+
